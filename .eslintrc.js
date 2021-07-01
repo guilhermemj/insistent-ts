6f8a20a62bba3c916c709f58538f34f9484a0892
Guilherme Jacomini <guilhermem.j@hotmail.com>
2021-07-01 13:04:16 -0300

    chore(deps): Bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
 
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
 
   parser: '@typescript-eslint/parser',
@@ -17,6 +17,14 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
   ],
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
 
   rules: {
   }
